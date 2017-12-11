ce22747af5dc3557eccb3275984ed686d1e7db28
truberton <gertandry@gmail.com>
2017-12-11 10:54:35 +0200

    Klassid ja asjad...

--- KonsooliMäng/KonsooliMäng/Program.cs
+++ KonsooliMäng/KonsooliMäng/Program.cs
@@ -10,11 +10,47 @@ namespace KonsooliMäng
     {
         static void Main(string[] args)
         {
+            Statid statid = new Statid();
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("The Mighty Adventures of Jaanus!");
-            Console.WriteLine("Made by\nMoka Studio™\n2017");
+            Console.WriteLine("Made by Moka Studio™ 2017\n");
+            while (true)
+            {
+                Console.WriteLine("You are the mighty adventurer Jaanus");
+                Console.WriteLine("Choose his class and title (same thing really)\n");
+                Console.WriteLine("*Knight\n*Mage\n*Archer\n*Rogue");
+                string klass = Console.ReadLine();
 
-
+                if (klass == "Knight" || klass == "knight")
+                {
+                    statid.Knight();
+                    Console.WriteLine("You are Jaanus the Knight");
+                    break;
+                }
+                else if (klass == "Mage" || klass == "mage")
+                {
+                    statid.Mage();
+                    Console.WriteLine("You are Jaanus the Mage");
+                    break;
+                }
+                else if (klass == "Archer" || klass == "archer")
+                {
+                    statid.Archer();
+                    Console.WriteLine("You are Jaanus the Archer");
+                    break;
+                }
+                else if (klass == "Rogue" || klass == "rogue")
+                {
+                    statid.Rogue();
+                    Console.WriteLine("You are Jaanus the Rogue");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Not an option (the fuck)\n");
+                }
+            }
+            Console.WriteLine("Töötab");
             Console.ReadLine();
         }
     }
--- KonsooliMäng/KonsooliMäng/Statid.cs
+++ KonsooliMäng/KonsooliMäng/Statid.cs
@@ -12,6 +12,7 @@ namespace KonsooliMäng
     {
         public int HP;
         public int MP;
+        public int ATK;
 
         //String klassiga saab klasside järgi panna mana ja hp jne.
 
@@ -19,24 +20,28 @@ namespace KonsooliMäng
         {
             HP = 20;
             MP = 5;
+            ATK = 12;
         }
 
         public void Mage()
         {
             HP = 15;
             MP = 25;
+            ATK = 7;
         }
 
         public void Archer()
         {
             HP = 17;
             MP = 7;
+            ATK = 10;
         }
 
         public void Rogue()
         {
             HP = 16;
             MP = 20;
+            ATK = 12;
         }
     }
 }
