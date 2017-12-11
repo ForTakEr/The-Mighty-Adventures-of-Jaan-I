ca9b24dbef5ba6d8041f1877a67001625aa14c3b
ForTakEr <32396272+ForTakEr@users.noreply.github.com>
2017-12-11 10:29:14 +0200

    Statid lisatud

--- KonsooliMäng/KonsooliMäng/Program.cs
+++ KonsooliMäng/KonsooliMäng/Program.cs
@@ -10,6 +10,12 @@ namespace KonsooliMäng
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("The Mighty Adventures of Jaanus!");
+            Console.WriteLine("Made by\nMoka Studio™\n2017");
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ KonsooliMäng/KonsooliMäng/Statid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsooliMäng
+{
+
+    
+    class Statid
+    {
+        public int HP;
+        public int MP;
+
+        //String klassiga saab klasside järgi panna mana ja hp jne.
+
+        public void Knight()
+        {
+            HP = 20;
+            MP = 5;
+        }
+
+        public void Mage()
+        {
+            HP = 15;
+            MP = 25;
+        }
+
+        public void Archer()
+        {
+            HP = 17;
+            MP = 7;
+        }
+
+        public void Rogue()
+        {
+            HP = 16;
+            MP = 20;
+        }
+    }
+}
