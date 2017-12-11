9bbd749fde2ddcce0edc63ba16be8c85854eed65
truberton <gertandry@gmail.com>
2017-12-11 11:16:05 +0200

    Algne story olemas
    IT BEGINS!

--- KonsooliMäng/KonsooliMäng/Program.cs
+++ KonsooliMäng/KonsooliMäng/Program.cs
@@ -47,10 +47,70 @@ namespace KonsooliMäng
                 }
                 else
                 {
-                    Console.WriteLine("Not an option (the fuck)\n");
+                    Console.WriteLine("Not an option (...the fuck?)\n");
                 }
             }
-            Console.WriteLine("Töötab");
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("You wake up in a strange forest, everything around you is dark and creepy... you hear thunder in the distance. You clutch your weapon in fear. It's frightening, what do you do?\n1. Look around\n2. Take a rest");
+                string response = Console.ReadLine();
+                if (response == "1")
+                {
+                    break;
+                }
+                else if (response == "2")
+                {
+                    Console.WriteLine("You have more important things to do than jack off to an extra curvy piece of driftwood that’s shaped like your old neighbours goat");
+                }
+                else
+                {
+                    Console.WriteLine("Please choose one of the options");
+                }
+            }
+            string path;
+            while (true)
+            {
+                Console.WriteLine("You notice 3 pathways in the distance, you go closer and notice an old ghost-man is in front of one of the paths, he proclaims: “One of these paths will bring you good fortune, the others only hardship CHOOSE WISELY FOR IT CANNOT BE CHANGED LATER!”\n1. Ask him for advice\n2. Choose the left path\n3. Choose the middle path\n4. Choose the right path\n");
+                path = Console.ReadLine();
+                if (path == "1")
+                {
+                    Console.WriteLine("“I won't tell you squat” he says");
+                    Console.WriteLine();
+                }
+                else if (path == "2")
+                {
+                    Console.WriteLine("You choose the left path");
+                    break;
+                }
+                else if (path == "3")
+                {
+                    Console.WriteLine("You choose the middle path");
+                    break;
+                }
+                else if (path == "4")
+                {
+                    Console.WriteLine("You choose the right path");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please choose one of the options");
+                }
+            }
+            if (path == "2") //SEE ON GERDI OMA
+            {
+
+            }
+            else if (path == "3") //SEE ON SIMO OMA
+            {
+
+            }
+            else if (path == "4") //SEE ON KERDI OMA
+            {
+
+            }
             Console.ReadLine();
         }
     }
